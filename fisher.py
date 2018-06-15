77f50cfd57b976a7a8fca0d5667aa171537fe26b
yangdepp <965093975@qq.com>
2018-06-16 00:55:28 +0800

    dev

--- fisher.py
+++ fisher.py
@@ -1,2 +1,19 @@
+from flask import Flask
+
 __author__ = 'yang'
-Flask 
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+
+# mvc的概念中，视图函数就是c的意思
+@app.route('/hello')
+def hello():
+    # 基于类的视图（即插视图）
+    return '<a href="http://baidu.com">哈哈哈</a>'
+
+
+if __name__ == '__main__':
+    # 生产环境下，fish-python不是入口文件
+    # 生产环境下，加此判断，不会启动flask服务器
+    app.run(host='0.0.0.0', debug=app.config['DEBUG'])
